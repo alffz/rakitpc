5bf9fd85d53d991996fa77e313c50549209f57c8
alffz <alfriansyah0408@gmail.com>
2024-05-16 23:15:02 +0700

    script deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://alffz.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/rakitpc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
